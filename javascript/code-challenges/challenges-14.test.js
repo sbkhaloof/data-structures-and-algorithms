2882ec5c1313aba43375127720b14c9eb5d168ab
siham khaloof <sehammalkawi92@gmail.com>
2021-08-10 23:40:56 +0300

    solve

--- javascript/code-challenges/challenges-14.test.js
+++ javascript/code-challenges/challenges-14.test.js
@@ -189,10 +189,17 @@ Here is a sample board:
 
 const detectTicTacToeWin = (board) => {
   // Solution code here...
-  // board.forEach(item=>{
-  //   for (let i=0;i<item.length;i++)
-  //   {if(item[i]=='x'||)}
-  // })
+  let x = board[0];
+  let y = board[1];
+  let z = board[2];
+  for (let i = 0; i < board.length; i++) {
+    if (x[0] == y[0] == z[0] || x[0] == y[1] == z[2] || x[2] == y[1] == z[0] || x[0] == x[1] == x[2] || y[0] == y[1] == y[2] || z[0] == z[1] == z[2] || x[1] == y[1] == z[1] || x[2] == y[2] == z[2]) {
+      return true
+    } else {
+      return false
+    }
+  }
+   return board;
 };
 
 /* ------------------------------------------------------------------------------------------------
