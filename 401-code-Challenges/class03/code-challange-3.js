2bf15fe6557d228cfea520497a2760a45828e16d
siham khaloof <sehammalkawi92@gmail.com>
2021-08-18 23:28:47 +0300

    edit code challenge 3

--- 401-code-Challenges/class03/code-challange-3.js
+++ 401-code-Challenges/class03/code-challange-3.js
@@ -1,11 +1,26 @@
 'use strict'
-function BinarySearch(arr,key){
-    for (let i=0;i<arr.length;i++)
-    if(arr[i]==key){return i;}
-     {return -1;}
+const binarySearch=(sortedArr,key)=>{
+    
+    let start = 0;
+    let end = sortedArr.length-1;
+    
+    while (start <= end) {
+        let mid = Math.ceil((start + end) / 2);
+        if (sortedArr[mid] === key) {
+            return mid;
+        } else if (sortedArr[mid] < key) {
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
+    }
+    return -1;
 }
-let result=BinarySearch([-131, -82, 0, 27, 42, 68, 179], 42);
-console.log(result);
+let array = [1,2,3,4,5];
+let key = 4
+console.log(`The index of ${key} is ${binarySearch(array,key)}`)
+
+
 
 
 
